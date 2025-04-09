d462def0966081435310e865f448d4d81e066c76
fkakatie <ksexton@adobe.com>
2025-04-09 13:50:33 -0600

    feat: update to latest

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,4 +1,5 @@
 import { getMetadata } from '../../scripts/aem.js';
+import { swapIcons } from '../../scripts/scripts.js';
 import { loadFragment } from '../fragment/fragment.js';
 
 /**
@@ -8,13 +9,24 @@ import { loadFragment } from '../fragment/fragment.js';
 export default async function decorate(block) {
   // load footer as fragment
   const footerMeta = getMetadata('footer');
-  const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : `${window.hlx.nfp}/footer`;
+  const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
   const fragment = await loadFragment(footerPath);
 
   // decorate footer DOM
   block.textContent = '';
-  const footer = document.createElement('div');
+  const footer = document.createElement('section');
+  footer.id = 'footer';
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
 
+  const classes = ['copyright', 'legal'];
+  classes.forEach((c, i) => {
+    const section = footer.children[i];
+    if (section) {
+      section.id = `footer-${c}`;
+      section.classList.add(`footer-${c}`);
+    }
+  });
+
   block.append(footer);
+  swapIcons(block);
 }
