f7af315f1c3dd425fdda8b2787a6cc22c9f79c01
David Nuescheler <david@nuescheler.ch>
2025-04-15 16:17:26 -0600

    chore: lint

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,21 +1,18 @@
-import { fetchPlaceholders } from '../../scripts/aem.js';
-
 export default function carousel(block) {
-   if (block.classList.contains('expansion')) {
-      const left = document.createElement('div');
-      left.classList.add('carousel-left');
-      const center = document.createElement('div');
-      center.classList.add('carousel-center');
-      const right = document.createElement('div');
-      right.classList.add('carousel-right');
-      left.append(block.firstElementChild);
+  if (block.classList.contains('expansion')) {
+    const left = document.createElement('div');
+    left.classList.add('carousel-left');
+    const center = document.createElement('div');
+    center.classList.add('carousel-center');
+    const right = document.createElement('div');
+    right.classList.add('carousel-right');
+    left.append(block.firstElementChild);
 
-      while (block.firstElementChild) {
-        console.log(block.firstElementChild);
-        right.append(block.firstElementChild);
-      }
-      block.appendChild(left);
-      block.appendChild(center);
-      block.appendChild(right);
-   }
+    while (block.firstElementChild) {
+      right.append(block.firstElementChild);
+    }
+    block.appendChild(left);
+    block.appendChild(center);
+    block.appendChild(right);
+  }
 }
