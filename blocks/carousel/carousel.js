c9baeb90d35b404d15231099f6edc8b731cc1f19
fkakatie <ksexton@adobe.com>
2025-04-21 19:54:55 -0600

    feat: expansion carousel

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,85 +1,40 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
 import { buildCarousel } from '../../scripts/scripts.js';
 
-function getVideoElement(source, replacePlaceholder, autoplay) {
-  const video = document.createElement('video');
-  video.setAttribute('controls', '');
-  video.dataset.loading = 'true';
-  video.addEventListener('loadedmetadata', () => delete video.dataset.loading);
-  if (autoplay || replacePlaceholder) {
-    video.setAttribute('autoplay', '');
-    if (autoplay) {
-      video.setAttribute('loop', '');
-      video.setAttribute('playsinline', '');
-      video.removeAttribute('controls');
-      video.addEventListener('canplay', () => {
-        video.muted = true;
-        video.play();
-      });
-    }
-  }
+/**
+ * Advances carousel to next slide
+ * @param {HTMLElement} carousel - Carousel element
+ */
+function nextSlide(carousel) {
+  const slides = [...carousel.children];
+  const current = slides.findIndex((s) => s.dataset.countdown);
+  const next = slides[(current + 1) % slides.length];
+  const desktop = window.matchMedia('width >= 800px').matches;
+  // scroll to next slide on mobile
+  if (!desktop) carousel.scrollTo({ left: next.offsetLeft, behavior: 'smooth' });
+  // show/hide "tabs" on desktop
+  slides.forEach((s) => s.removeAttribute('data-countdown'));
+  // set countdown on next slide
+  next.dataset.countdown = true;
+}
 
-  const sourceEl = document.createElement('source');
-  sourceEl.setAttribute('src', source);
-  sourceEl.setAttribute('type', `video/${source.split('.').pop()}`);
-  video.append(sourceEl);
+/**
+ * Enable automatic carousel rotation
+ * @param {HTMLElement} carousel - Carousel element
+ * @param {number} interval - Time (in ms) between slide transitions
+ * @returns {number} - Interval ID
+ */
+function autoRotate(carousel, interval = 6000) {
+  const slides = [...carousel.children];
+  if (slides.length <= 1) return;
 
-  return video;
+  // eslint-disable-next-line consistent-return
+  return setInterval(() => {
+    nextSlide(carousel);
+  }, interval);
 }
 
 export default function decorate(block) {
-  /* expansion */
-  if (block.classList.contains('expansion')) {
-    const left = document.createElement('div');
-    left.classList.add('carousel-left');
-    const center = document.createElement('div');
-    center.classList.add('carousel-center');
-    const right = document.createElement('div');
-    right.classList.add('carousel-right');
-    left.append(block.firstElementChild);
-
-    const selectSlide = (slide) => {
-      const videoHref = slide.dataset.video;
-      const imageSrc = slide.dataset.image;
-
-      [...slide.parentElement.children].forEach((child) => {
-        child.ariaSelected = false;
-      });
-      slide.ariaSelected = true;
-
-      if (videoHref) {
-        const videoElement = getVideoElement(videoHref, true, true);
-        center.replaceChildren(videoElement);
-      }
-
-      if (imageSrc) {
-        const imageElement = createOptimizedPicture(imageSrc);
-        center.replaceChildren(imageElement);
-      }
-    };
-
-    while (block.firstElementChild) {
-      const slide = block.firstElementChild;
-      const video = block.firstElementChild.querySelector('a[href$=".mp4"]');
-      if (video) {
-        slide.dataset.video = video.href;
-        video.remove();
-      }
-      const image = block.firstElementChild.querySelector('img');
-      if (image) {
-        slide.dataset.image = image.src;
-        image.parentElement.remove();
-      }
-      slide.addEventListener('click', () => selectSlide(slide));
-      right.append(block.firstElementChild);
-    }
-    block.appendChild(left);
-    block.appendChild(center);
-    block.appendChild(right);
-
-    selectSlide(right.firstElementChild);
-    return;
-  }
+  const variants = [...block.classList].filter((c) => c !== 'block' && c !== 'carousel');
 
   const rows = [...block.children];
   block.innerHTML = '';
@@ -90,17 +45,95 @@ export default function decorate(block) {
 
   // extract slides
   const slides = rows.map((s) => s.children);
+  let caption;
+
+  // decorate expansion variant
+  if (variants.includes('expansion')) {
+    variants.forEach((v) => block.parentElement.classList.add(`${v}-wrapper`));
+
+    // add logo icon
+    const logo = document.createElement('img');
+    logo.className = 'expansion-logo';
+    logo.src = '/icons/mark.svg';
+    block.parentElement.prepend(logo);
+
+    // extract first "slide" as caption
+    [caption] = slides.shift();
+    caption.classList.add('carousel-caption');
+
+    // track visible slide
+    wrapper.addEventListener('scroll', () => {
+      const { scrollLeft, clientWidth } = wrapper;
+      const current = Math.round(scrollLeft / clientWidth);
+      const { children } = wrapper;
+      [...children].forEach((slide, i) => {
+        if (i === current) slide.setAttribute('data-countdown', true);
+        else slide.removeAttribute('data-countdown');
+      });
+    });
+  }
+
   slides.forEach((s) => {
     const slide = document.createElement('li');
     [...s].forEach((cell) => {
-      const img = cell.querySelector('div img, div svg');
-      if (img) cell.classList.add('img-wrapper');
+      const vid = cell.querySelector('a[href$=".mp4"]');
+      if (vid) {
+        // load video
+        const video = document.createElement('video');
+        video.autoplay = true;
+        video.muted = true;
+        video.loop = true;
+        const source = document.createElement('source');
+        source.src = vid.href;
+        source.type = 'video/mp4';
+        video.append(source);
+        vid.parentElement.replaceWith(video);
+        video.play();
+      }
       slide.append(cell);
     });
+
+    // add expansion autotimer
+    if (variants.includes('expansion')) {
+      const timer = document.createElement('div');
+      timer.classList.add('expansion-timer');
+      const heading = slide.querySelector('h2, h3, h4, h5, h6');
+      if (heading) heading.prepend(timer);
+      else slide.prepend(timer);
+    }
+
     wrapper.append(slide);
   });
 
+  // start autorotation
+  if (variants.includes('expansion')) {
+    const firstSlide = wrapper.firstElementChild;
+    firstSlide.dataset.countdown = true;
+
+    let autoRotateTimer = autoRotate(wrapper);
+    let interactionTimeout;
+
+    const resetAutoRotate = () => {
+      clearInterval(autoRotateTimer);
+      clearTimeout(interactionTimeout);
+      interactionTimeout = setTimeout(() => {
+        autoRotateTimer = autoRotate(wrapper);
+      }, 100); // match scroll debounce
+    };
+
+    wrapper.addEventListener('scroll', resetAutoRotate);
+
+    [...wrapper.children].forEach((slide) => {
+      slide.addEventListener('click', () => {
+        wrapper.querySelectorAll('li').forEach((s) => s.removeAttribute('data-countdown'));
+        slide.setAttribute('data-countdown', true);
+        resetAutoRotate();
+      });
+    });
+  }
+
   const carousel = buildCarousel(block, 1, false);
+  if (caption) carousel.parentElement.prepend(caption);
 
   if (carousel) block.replaceWith(carousel);
   else block.parentElement.remove();
