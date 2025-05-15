0a456a1d85d464af2ddacc7892a9d3b16fa149e6
David Nuescheler <david@nuescheler.ch>
2025-05-15 14:17:22 -0600

    chore: remove legacy tags

--- scripts/consented.js
+++ scripts/consented.js
@@ -11,9 +11,6 @@ window.config = {
 loadScript('https://www.vitamix.com/etc.clientlibs/vitamix/clientlibs/clientlib-chatbot.lc-5aa4591c22ce9007a60fafc1f19aa690-lc.min.js');
 loadScript('https://www.vitamix.com/etc.clientlibs/core/wcm/components/commons/site/clientlibs/container.lc-0a6aff292f5cc42142779cde92054524-lc.min.js');
 loadScript('https://assets.adobedtm.com/launch-EN40f2d69539754c3ea73511e70c65c801.min.js');
-loadScript('https://js.adsrvr.org/up_loader.1.1.0.js');
-
-loadScript('https://www.vitamix.com/etc.clientlibs/vitamix/clientlibs/clientlib-chatbot.lc-5aa4591c22ce9007a60fafc1f19aa690-lc.min.js');
 
 await loadScript('https://www.vitamix.com/etc.clientlibs/vitamix/clientlibs/clientlib-library.lc-259cf15444c5fe1f89e5c54df7b6e1e9-lc.min.js');
 loadScript('https://www.vitamix.com/etc.clientlibs/vitamix/clientlibs/clientlib-analytics.lc-26814920488a848ff91c1f425646d010-lc.min.js');
@@ -31,45 +28,6 @@ loadScript('https://www.googletagmanager.com/gtag/js?id=G-XJB3SPQE38');
 gtag('js', new Date());
 gtag('config', 'G-XJB3SPQE38');
 
-
-loadScript('https://insight.adsrvr.org/track/up', () => {
-  ttd_dom_ready(() => {
-    if (typeof TTDUniversalPixelApi === 'function') {
-      const universalPixelApi = new TTDUniversalPixelApi();
-      universalPixelApi.init('um3hqnt', ['hqj90x6'], 'https://insight.adsrvr.org/track/up');
-    }
-  });
-});
-
-
-(function (d) {
-  if (window.__cbtagLoaded) { return; }
-  window.__cbtagLoaded = true;
-  if (!window.cbtag) { window.cbtagdata = []; window.cbtag = function (event, args) { cbtagdata.push({ event, args }); }; }
-  d.addEventListener('DOMContentLoaded', () => {
-    var e = function (u) { return encodeURIComponent(u); };
-    const cMacro = '';
-    const sourceUrl = e('');
-    const lidJson = e(JSON.stringify({
-      a: 'www.vitamix.com',
-    }));
-    const ifr = (function () { try { return (!top.location.href ? 1 : 0); } catch (e) { return 1; } }());
-    const isSF = (function () { if (window.$sf) { if ($sf.ext && $sf.ext.supports) { const s = $sf.ext.supports(); return s && s['exp-ovr'] ? 'sfexp' : 'sf'; } return 'sf'; } return 'nosf'; }());
-    const ref = (function (w) {
-      try {
-        while (w.parent && w.parent !== window.top) {
-          if (!w.parent.document) { break; } w = w.parent;
-        }
-      } catch (ignore) { } return w.document.referrer;
-    }(window));
-    var e = function (u) { return encodeURIComponent(u); }; const tid = `cbox_ph_${Math.floor(Math.random() * 10e6)}`; let h;
-    const src = `https://ws.contobox.com/getcode.js?ph_id=${
-      tid}&brandID=5393&websiteID=119&zoneID=176739&adID=176247&clientparam=&lid=${lidJson}&sourceUrl=${e(sourceUrl)}&ifr=${ifr}&isSF=${isSF}&clicktag=${e(cMacro)}&fromurl=${
-      e(location.href)}&nomraid=true&ref=${e(ref)}`; const
-      s = d.createElement('script'); s.type = 'text/javascript';
-    s.async = true; s.src = src; d.body.appendChild(s);
-  });
-}(document));
 
 loadScript('https://s-a.innovid.com/conversion/1hk0tl');
 loadScript('https://arttrk.com/pixel/?ad_log=referer&action=content&pixid=82dc3545-14a0-41d8-9870-2156059087d9');
@@ -94,26 +52,6 @@ fbq('init', '1597403650511067');
 fbq('track', 'PageView');
 
 // End Facebook Pixel Code
-// AcuityAds Pixel
-    'use strict'
-    !(function(a, e) {
-        if (!a.aap) {
-            a.aap = function(e) {
-                ;(a.acuityAdsEventQueue = a.acuityAdsEventQueue || []).push(e)
-            }
-            var t = 'script'
-            var i = e.createElement(t)
-            i.async = true
-            i.src = 'https://origin.acuityplatform.com/event/v2/pixel.js'
-            var c = e.getElementsByTagName(t)[0]
-            c.parentNode.insertBefore(i, c)
-            a.acuityPiggybackCallback = function(e) {
-                a.acuityParseResponse(e)
-            }
-        }
-    })(window, document)
-    aap({ pixelKey: '5133925694833979260' })
-// End of AcuityAds Pixel // End of global snippet
 
 // Snap Pixel Code
 try {
