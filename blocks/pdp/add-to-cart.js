25f4764b9b8316441a4c6f113cb0cc5130930a46
Dylan Depass <dylandepass@gmail.com>
2025-06-05 11:59:15 -0400

    fix: try loading cartApi directly from uat

--- blocks/pdp/add-to-cart.js
+++ blocks/pdp/add-to-cart.js
@@ -1,4 +1,5 @@
-import { cartApi } from '../../scripts/minicart/api.js';
+// eslint-disable-next-line import/no-unresolved
+import { cartApi } from 'https://uat.vitamix.com/scripts/minicart/api.js';
 
 export default async function addToCart(sku, options, quantity) {
   cartApi.addToCart(sku, options, quantity);
