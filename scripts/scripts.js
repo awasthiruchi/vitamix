d9bc556d41e3cbdf92144cbad88e26adee9d609f
David Nuescheler <david@nuescheler.ch>
2025-06-11 16:22:39 -0600

    chore: reflect stock properly

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -252,8 +252,7 @@ function parseVariants(sections) {
 
 // eslint-disable-next-line no-unused-vars
 export function checkOutOfStock(sku) {
-  // const { availability } = window.jsonLdData.offers.find((offer) => offer.sku === sku);
-  const availability = 'https://schema.org/InStock';
+  const { availability } = window.jsonLdData.offers.find((offer) => offer.sku === sku);
   return availability === 'https://schema.org/OutOfStock';
 }
 
