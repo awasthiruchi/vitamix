e774b1b08978d11b03670918eb425ce1f80a8ef8
dylandepass <dylandepass@gmail.com>
2025-10-22 13:41:32 -0400

    fix: bundle variant options should update images

--- blocks/pdp/options.js
+++ blocks/pdp/options.js
@@ -38,8 +38,7 @@ export function onOptionChange(block, variants, color) {
   selectedOptionLabel.textContent = `Color: ${variantColor}`;
 
   // check if bundle (should skip variant images)
-  const bundle = getMetadata('type') === 'bundle';
-  let variantImages = bundle ? [] : variant.images || [];
+  let variantImages = variant.images || [];
   variantImages = [...variantImages].map((v, i) => {
     const clone = v.cloneNode(true);
     clone.dataset.source = i ? 'variant' : 'lcp';
